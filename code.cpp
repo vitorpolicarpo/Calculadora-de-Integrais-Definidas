65f8e0b5efa085360358fe1b0392d70f04959c26
vitorpolicarpo <vitorpcaetano06@gmail.com>
2025-09-19 16:41:01 -0300

    Update code.cpp
    Foram adicionados saídas que ajudam a interação com o usuário, tal qual descrito no vídeo

--- code.cpp
+++ code.cpp
@@ -7,8 +7,11 @@ int main(){
 double a, b; // a e b fazem parte dos intervalos
 int n; // o número de divisões feitas
 
+cout << "Digite o início do intervalo (a):";
 cin >> a;
+cout << "Digite o início do intervalo (b):";
 cin >> b;
+cout << "Digite o número de subdivisões (n):";
 cin >> n; // quanto maior o n, mais próxima é a aproximação
 
 double base = (b - a) / n; // com base no intervalo, achamos a base dos retângulos
@@ -19,11 +22,12 @@ double soma = 0.0;
 
 for (int i = 0; i < n; i++){
     double p = a + (i + 0.5) * base; // achamos o ponto médio do intervalo a esquerda
-    soma = soma + (p * p + 2 * p + 4) ; // colocamos a função com o ponto médio: f(p)
+    soma = soma + 1/p; // colocamos a função com o ponto médio: f(p)
 }
 
 double calculo_area = soma * base;
 
+cout << "Resposta: ";
 cout << calculo_area;
 
 }
